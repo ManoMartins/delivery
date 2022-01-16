ca55d312b67282df5e9ad5b41b6715c0595cfffe
Manoel Martins <mano.martins29@gmail.com>
2022-01-16 10:39:31 -0300

    :sparkles: add create delivery

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,24 @@
 import { Router } from "express"
-import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController"
-import { AuthenticateDeliverymanController } from "./modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController"
+import { CreateDeliveryController } from "./modules/deliveries/useCases/CreateDeliveryController"
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController"
+import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController"
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController"
+import { AuthenticateDeliverymanController } from "./modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController"
 
 const routes = Router()
 
 const createClientController = new CreateClientController()
+const createDeliveryController = new CreateDeliveryController()
 const createDeliverymanController = new CreateDeliverymanController()
 const authenticateClientController = new AuthenticateClientController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 
-routes.post("/clients/authenticate", authenticateClientController.handle)
-routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle)
+routes.post("/delivery", createDeliveryController.handle)
+
 routes.post("/clients", createClientController.handle)
+routes.post("/clients/authenticate", authenticateClientController.handle)
+
 routes.post("/deliveryman", createDeliverymanController.handle)
+routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle)
 
 export { routes }
